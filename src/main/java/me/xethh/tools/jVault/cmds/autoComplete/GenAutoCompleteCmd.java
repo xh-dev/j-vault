88b7b11b38471b74b38280e19a23a5203885d6f2
xeth hung <xethhung@xh-network.xyz>
2024-05-11 13:11:53 +0800

    Add auto complete

---
+++ src/main/java/me/xethh/tools/jVault/cmds/autoComplete/GenAutoCompleteCmd.java
@@ -0,0 +1,19 @@
+package me.xethh.tools.jVault.cmds.autoComplete;
+
+import me.xethh.tools.jVault.Main;
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(
+        name = "autocomplete",
+        description = "manage token"
+)
+public class GenAutoCompleteCmd implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        final var bashScript = picocli.AutoComplete.bash("j-vault", new CommandLine(new Main()));
+        System.out.println(bashScript);
+        return 0;
+    }
+}
