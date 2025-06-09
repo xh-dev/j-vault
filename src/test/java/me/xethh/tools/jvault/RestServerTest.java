85ed3b4957446609182a309fda1489948349c05c
xeth hung <xethhung@xh-network.xyz>
2025-06-10 07:43:50 +0800

    update

--- src/test/java/me/xethh/tools/jvault/RestServerTest.java
+++ src/test/java/me/xethh/tools/jvault/RestServerTest.java
@@ -3,6 +3,7 @@ package me.xethh.tools.jvault;
 import io.vavr.control.Try;
 import me.xethh.libs.encryptDecryptLib.jwtVer.Sys;
 import net.lingala.zip4j.ZipFile;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +17,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static me.xethh.tools.jvault.authserv.AuthServerClient.TestConst.TEST_TOKEN;
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,8 +25,96 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Test vault rest server")
 public class RestServerTest {
     @Test
-    @DisplayName("When run vault rest server")
-    public void testVaultSetOverride() {
+    @DisplayName("When run vault rest server with secure mode")
+    public void testSecureRestServer() {
+        var streams = PasswordGenTest.streamsWithPipe();
+        PasswordGenTest.borrowStdOutV3(streams._1(), streams._2(), streams._3(), () -> {
+            var f1 = Path.of("target/test-case/vault.kv");
+            if (f1.toFile().exists())
+                f1.toFile().delete();
+            f1.getParent().toFile().mkdirs();
+            var cmd = String.format("vault -c %s set -f %s -k x -v y", TEST_TOKEN, f1.toString());
+            Main.main(cmd.split(" "));
+
+            var tokenGen = new AtomicReference<String>(null);
+            new Thread(() -> {
+                var cmd1 = String.format("vault -c %s over-http --secure -f %s ", TEST_TOKEN, f1.toString());
+                Main.main(cmd1.split(" "));
+            }).start();
+            var client = HttpClient.newHttpClient();
+            HttpResponse res = null;
+
+            final int MAX=10;
+            int i=0;
+            boolean success = false;
+            while (i<MAX) {
+                var out = streams._2().toString().split("\n");
+                if(out.length > 1){
+                    var found_token=false;
+                    for(var str : out){
+                        if(str.trim().equalsIgnoreCase("token:")){
+                            found_token=true;
+                            continue;
+                        }
+                        if(found_token){
+                            tokenGen.set(str);
+                            success = true;
+                            break;
+                        }
+                    }
+                }
+                if(tokenGen.get() != null){
+                    break;
+                }
+                Thread.sleep(Duration.ofSeconds(1).toMillis());
+                i++;
+            }
+            res = client.send(HttpRequest.newBuilder(new URI("http://localhost:7910/kv/x?token="+tokenGen)).GET().build(), HttpResponse.BodyHandlers.ofString());
+            assertEquals("y", res.body());
+        });
+    }
+
+    @Test
+    @DisplayName("When run vault rest server with harden mode")
+    public void testHardenRestServer() {
+        var streams = PasswordGenTest.streamsWithPipe();
+        PasswordGenTest.borrowStdOutV3(streams._1(), streams._2(), streams._3(), () -> {
+            var f1 = Path.of("target/test-case/vault.kv");
+            if (f1.toFile().exists())
+                f1.toFile().delete();
+            f1.getParent().toFile().mkdirs();
+            var cmd = String.format("vault -c %s set -f %s -k x -v y", TEST_TOKEN, f1.toString());
+            Main.main(cmd.split(" "));
+            new Thread(() -> {
+                var cmd1 = String.format("vault -c %s over-http --harden -f %s ", TEST_TOKEN, f1.toString());
+                Main.main(cmd1.split(" "));
+            }).start();
+            var client = HttpClient.newHttpClient();
+            HttpResponse res = null;
+            var tokenGen = DigestUtils.md5Hex(String.format("http://0.0.0.0:%s", "7910"));
+
+            final int MAX=10;
+            int i=0;
+            boolean success = false;
+            while (i<MAX) {
+                try {
+                    res = client.send(HttpRequest.newBuilder(new URI("http://localhost:7910/kv/x?token="+tokenGen)).GET().build(), HttpResponse.BodyHandlers.ofString());
+                    assertEquals("y", res.body());
+                    success = true;
+                    break;
+                } catch (ConnectException x) {
+                    // ignored
+                }
+                Thread.sleep(Duration.ofSeconds(1).toMillis());
+                i++;
+            }
+            assertTrue(success);
+        });
+    }
+
+    @Test
+    @DisplayName("When run vault rest server with normal case")
+    public void testBasicRestServer() {
         var streams = PasswordGenTest.streamsWithPipe();
         PasswordGenTest.borrowStdOutV3(streams._1(), streams._2(), streams._3(), ()->{
             var f1= Path.of("target/test-case/vault.kv");
@@ -33,15 +123,6 @@ public class RestServerTest {
             f1.getParent().toFile().mkdirs();
             var cmd = String.format("vault -c %s set -f %s -k x -v y", TEST_TOKEN, f1.toString());
             Main.main(cmd.split(" "));
-            //cmd = String.format("vault -c %s over-http -f %s ", TEST_TOKEN, f1.toString());
-            //new Thread(() -> {
-            //    Try.of(()->{
-            //        streams._2().write("Started shutdown helper".getBytes(StandardCharsets.UTF_8));
-            //        Thread.sleep(Duration.ofSeconds(5).toMillis());
-            //        streams._1().write("shutdonw".getBytes(StandardCharsets.UTF_8));
-            //        return 1;
-            //    }).get();
-            //}).start();
             new Thread(() -> {
                 var cmd1 = String.format("vault -c %s over-http -f %s ", TEST_TOKEN, f1.toString());
                 Main.main(cmd1.split(" "));
